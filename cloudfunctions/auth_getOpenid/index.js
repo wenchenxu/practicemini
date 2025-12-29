ac7dc187a3a08ba328a2c3990891bbe765b6750a
wenchenxu <xu.steven.wc@gmail.com>
2025-12-30 00:06:07 +0800

    delete obsolete cloud functions

--- cloudfunctions/auth_getOpenid/index.js
+++
@@ -1,7 +0,0 @@
-// cloudfunctions/getOpenid/index.js
-const cloud = require('wx-server-sdk');
-cloud.init();
-exports.main = async () => {
-  const { OPENID } = cloud.getWXContext();
-  return { openid: OPENID };
-};
